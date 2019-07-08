f648fbeb61d814cfeec5e3b798fccee4d614a9bd
Olivia <oliviastarkweather16@gmail.com>
2019-07-08 13:53:31 -0500

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+//var name1 = "Ellen"
+//var name2 = "Hunter"
+//
+//print("Hi \(name1) and \(name2)!")
+//
+//var names = name1 + " and " + name2
+//print("Hi \(names)!")
+
+
+
+//Working with integers and doubes
+
+var int = 4
+var doub = 5.0
+
+print(Double(int) * doub)
+
+
+var a = 12.0
+var b = 65
+var c = 31
+var d = 98
+
+var ave = (a + Double(b + c + d)) / 4.0
+
+print(ave)
+
+
